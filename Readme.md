27014b83fb6419e32263298927c586d9f9629b04
Rajganez <ganezsouth@live.com>
2024-06-10 19:10:48 +0530

    Created API endpoints using express.js

---
+++ Readme.md
@@ -0,0 +1,22 @@
+## Description
+# Technology used - Node JS
+
+***API Endpoints created using express.js***
+
+***Two Endpoint Created***
+
+> /create-file -- Creates a file with the file name in Current Date and Time, with the timestamp as the content
+
+> /read-files -- Reads all files with their filename
+
+## Installation
+
+1. Create folder in the local directory
+
+2. Using cmd in the created folder and type > npm init
+
+3. Enter the package name and proceed with all the steps
+
+4. Open with VSCode and package.JSON file is created
+
+5. Install Express.js using > npm i express command
---
+++ fs-utils.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+
+//Converted Date-Time to file name format
+const date = new Date().toString();
+let textDate = date.toString();
+let txt = textDate.split(":").join("-");
+
+//Function to Create Directory and Write file inside the directory
+const createDir = () => {
+  try {
+    if (!fs.existsSync("files")) {
+      // If file doesn't exist create directory
+      fs.mkdirSync("files");
+    }
+    // Write file with file name in Date format and content with current timestamp
+    fs.writeFileSync(`./files/${txt}.txt`, `TimeStamp : ${Date.now()}`);
+  } catch (error) {
+    console.log(`Error creating Dir : ${error.message}`);
+  }
+};
+
+//Function to Read Folder with the file name
+const readFolder = (folderName) => {
+  try {
+    const files = fs.readdirSync(folderName);
+    return files;
+  } catch (error) {
+    console.log(`Error reading: ${error.message}`);
+  }
+};
+
+export { createDir, readFolder };
