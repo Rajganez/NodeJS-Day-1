258bd20a93e8b642aeae89f732292b2e866777ba
Rajganez <ganezsouth@live.com>
2024-06-15 10:13:36 +0530

    Converted Date Time format to Save as a file name

--- fs-utils.js
+++ fs-utils.js
@@ -1,10 +1,5 @@
 import fs from "fs";
 
-//Converted Date-Time to file name format
-let date = new Date().toString();
-let textDate = date.toString();
-let txt = textDate.split(":").join("-");
-
 //Function to Create Directory and Write file inside the directory
 const createDir = () => {
   try {
@@ -12,6 +7,10 @@ const createDir = () => {
       // If file doesn't exist create directory
       fs.mkdirSync("files");
     }
+    //Converted Date-Time to file name format
+    let date = new Date().toString();
+    let textDate = date.toString();
+    let txt = textDate.split(":").join("-");
     // Write file with file name in Date format and content with current timestamp
     fs.writeFileSync(`./files/${txt}.txt`, `TimeStamp : ${Date.now()}`);
   } catch (error) {
